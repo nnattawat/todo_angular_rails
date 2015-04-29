dec9d5f822931d42493033a914614f015b44ede1
Nattawat Nonsung <armmer1@gmail.com>
2015-04-29 11:31:58 +1000

    Refactor resources

--- app/assets/javascripts/todo/controllers.js
+++ app/assets/javascripts/todo/controllers.js
@@ -4,18 +4,19 @@ var todoControllers = angular.module('todoControllers', [
   'todoServices'
 ]);
 
-todoControllers.controller('TodoItemsController', ['$scope', 'TodoItems', 'TodoItem',
-  function ($scope, TodoItems, TodoItem) {
+todoControllers.controller('TodoItemsController', ['$scope', 'TodoItem',
+  function ($scope, TodoItem) {
 
-    $scope.todoItems = TodoItems.query();
+    $scope.todoItems = TodoItem.query();
     $scope.orderProp = 'created_at';
+    $scope.todo = new TodoItem();
 
     $scope.addTodoItem = function() {
       $scope.todo.completed = false;
-      TodoItems.create({todo_item: $scope.todo}, function( data ){
+      $scope.todo.$save(function(data) {
         $scope.hideForm();
-        $scope.todoItems.push(data.toJSON());
-      }, function(error){
+        $scope.todoItems.push(new TodoItem(data.toJSON()));
+      }, function(error) {
         console.log(error)
         alert(error.data.join(' and '));
       });
@@ -23,8 +24,8 @@ todoControllers.controller('TodoItemsController', ['$scope', 'TodoItems', 'TodoI
 
     $scope.remove = function(todo) {
       if (confirm("Are you sure?")) {
-        TodoItem.delete({ id: todo.id }, function(){
-          $scope.todoItems = TodoItems.query();   // after deleted, fetch collection.
+        todo.$delete(function() {
+          $scope.todoItems = TodoItem.query();   // after deleted, fetch collection.
         });
       }
     };
@@ -45,13 +46,11 @@ todoControllers.controller('TodoItemsController', ['$scope', 'TodoItems', 'TodoI
 todoControllers.controller('TodoItemController', ['$scope', '$routeParams', 'TodoItem',
   function ($scope, $routeParams, TodoItem) {
 
-    $scope.todo = TodoItem.get({ id: $routeParams.todoId }, function(todo) {
-    });
+    $scope.todo = TodoItem.get({ id: $routeParams.todoId });
 
     $scope.completeTodoItem = function() {
       $scope.todo.completed = true;
-      TodoItem.update({ id: $scope.todo.id, todo_item: $scope.todo }, function(){
-      });
+      $scope.todo.$update();
     };
 
   }]);
--- app/assets/javascripts/todo/services.js
+++ app/assets/javascripts/todo/services.js
@@ -4,19 +4,8 @@ var todoServices = angular.module('todoServices', [
   'ngResource'
 ]);
 
-todoServices.factory('TodoItems', ['$resource',
-  function($resource){
-    return $resource('/todo_items.json', {}, {
-      query: { method: 'GET', isArray: true },
-      create: { method: 'POST' }
-    });
-  }]);
-
-todoServices.factory('TodoItem', ['$resource',
-  function($resource){
-    return $resource('/todo_items/:id.json', {}, {
-      show: { method: 'GET' },
-      update: { method: 'PUT', params: {id: '@id'} },
-      delete: { method: 'DELETE', params: {id: '@id'} }
-    });
-  }]);
+todoServices.factory('TodoItem', ['$resource', function($resource){
+  return $resource('/todo_items/:id.json', { id: '@id' }, {
+    'update': { method: 'PATCH'  }
+  });
+}]);
